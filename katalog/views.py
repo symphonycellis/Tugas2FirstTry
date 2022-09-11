9f3687fc9eb6a6951dc1299b25f61f5feea8253f
symphonycellis <symphony.cellis@ui.ac.id>
2022-09-11 13:13:20 +0700

    First Commit Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'Name' : 'Symphony Cellis Zaana Saraaya',
+        'StudentID' : '2106752060',
+        'list_barang' : data_barang_katalog
+    }
+    return render(request, "katalog.html", context)
